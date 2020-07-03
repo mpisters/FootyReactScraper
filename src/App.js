1e01802bf0a7efd20a83a73c40f31e73d0744d62
mpisters <pistersmichelle@gmail.com>
2020-07-03 23:20:54 +0200

    finish table

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import scrapeFooty from './scrape';
 class App extends Component {
   state = {
     scores: [],
-    columnNames: []
+    columnNames: [],
   };
 
   async componentDidMount() {
@@ -13,13 +13,17 @@ class App extends Component {
         .then(response => {
           if (response.data) {
             this.setState((state) => {
-              let scores = scrapeFooty(response.data.htmlPage)
-              let columnNames = Object.keys(scores[0])
+              let scores = scrapeFooty(response.data.htmlPage);
+              let columnNames = Object.keys(scores[0]);
               return {scores, columnNames};
             });
           }
         })
         .catch(console.error);
+  }
+
+  getTeamValues(team) {
+    return Object.values(team);
   }
 
   render() {
@@ -29,11 +33,17 @@ class App extends Component {
       <table className="table">
         <thead>
         <tr>
-          {this.state.columnNames.map((name, key) => {return <th key={key}>{name}</th>})}
+          {this.state.columnNames.map((name, key) => {
+            return <th key={key}>{name}</th>;
+          })}
         </tr>
         </thead>
         <tbody>
-          {this.state.scores.map((team, key) => {return <tr key={key}>{JSON.stringify(team)}</tr>})}
+        {this.state.scores.map((team, key) => {
+          return <tr key={key}>{this.getTeamValues(team).map((value, key) => {
+            return <td key={key}>{value}</td>;
+          })}</tr>;
+        })}
         </tbody>
       </table>
     </div>;
