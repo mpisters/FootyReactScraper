02e77303cabe2000cbc4acf12ae6a305372946d6
mpisters <pistersmichelle@gmail.com>
2020-07-03 23:12:13 +0200

    create table for footy data

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
 import React, {Component} from 'react';
 import axios from 'axios';
+import scrapeFooty from './scrape';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.handleClick.bind(this);
-  }
+  state = {
+    scores: [],
+    columnNames: []
+  };
 
-  async handleClick() {
+  async componentDidMount() {
     await axios.get('/api/scrape/')
         .then(response => {
-          console.log("RESPONSE", response)
+          if (response.data) {
+            this.setState((state) => {
+              let scores = scrapeFooty(response.data.htmlPage)
+              let columnNames = Object.keys(scores[0])
+              return {scores, columnNames};
+            });
+          }
         })
         .catch(console.error);
-
   }
 
   render() {
     return <div className="container">
-      <button onClick={this.handleClick}>Scrape Footy</button>
+      <h1>Scraped Footy</h1>
+      {this.scores}
+      <table className="table">
+        <thead>
+        <tr>
+          {this.state.columnNames.map((name, key) => {return <th key={key}>{name}</th>})}
+        </tr>
+        </thead>
+        <tbody>
+          {this.state.scores.map((team, key) => {return <tr key={key}>{JSON.stringify(team)}</tr>})}
+        </tbody>
+      </table>
     </div>;
   }
 }
---
+++ src/scrape.js
@@ -0,0 +1,39 @@
+let scrapeFooty = (htmlPage) => {
+  let divElement = document.createElement('div');
+  divElement.innerHTML = htmlPage;
+  let banner = divElement.querySelectorAll('section#banner')[0];
+  let schema = banner.querySelector('table');
+  let columnHeaders = schema.getElementsByTagName('th');
+  let columnNames = getColumnNames(columnHeaders);
+  let tbody = schema.getElementsByTagName('tbody')[0];
+  let rows = tbody.getElementsByTagName('tr');
+  let scores = getScores(rows, columnNames);
+  return scores;
+};
+
+let getColumnNames = (columnHeaders) => {
+  let columnNames = [];
+  for (let item of columnHeaders) {
+    columnNames.push(item.textContent);
+  }
+  return columnNames;
+};
+
+
+let getScores = (rows, keys) => {
+
+  let scores = [];
+  for (let row of rows) {
+    let values = row.childNodes;
+    let team = {};
+    for (let i = 0; i < values.length; i++) {
+      let value = values[i];
+      team[keys[i]] = value.textContent;
+    }
+    scores.push(team);
+  }
+  return scores;
+};
+
+export default scrapeFooty;
+
