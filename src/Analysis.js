a925985ed3fcc6302057b3c23facdf966bec54bc
mpisters <pistersmichelle@gmail.com>
2020-07-13 22:28:55 +0200

    add highcharts

--- src/Analysis.js
+++ src/Analysis.js
@@ -1,8 +1,26 @@
 import React, {Component} from 'react';
+import HighCharts from 'highcharts';
+import axios from 'axios';
 
 class Analysis extends Component {
+  async getScores() {
+    console.log('GET the scores');
+    return await axios.get('/api/scores/').then((response) => {return response});
+  }
+
+  async componentDidMount() {
+    const scoresResponse = await this.getScores();
+    console.log(scoresResponse.data);
+    HighCharts.chart('scores', {
+      title: {text: 'Scores per team'},
+    });
+  }
+
   render() {
-    return <h1>Analysis</h1>;
+    return <div className="analysis-page">
+      <h1>Analysis</h1>
+      <div id="scores"></div>
+    </div>;
   }
 }
 
